86e9df928f27c4d5236430ea3dccabef40e8b542
Kong Jirawong <greannetwork@gmail.com>
2015-06-05 11:46:26 +0700

    add redis

---
+++ src/main/java/com/mockupcode/springboot/docker/config/HttpSessionConfig.java
@@ -0,0 +1,12 @@
+package com.mockupcode.springboot.docker.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ *
+ * @author Jirawong Wongdokpuang <greannetwork@gmail.com>
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+    
+}
